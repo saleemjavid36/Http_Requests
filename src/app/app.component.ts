0d3e392a668040c633153014cd86c021d0658fc6
saleemjavid36 <saleemjavid360@gmail.com>
2023-01-31 18:21:18 +0530

    6_settingPostreq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,10 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
-
+    this.http.post('https://ng-complete-guide-e481e-default-rtdb.firebaseio.com/posts.json',postData)
+    .subscribe(responseData=>{
+      console.log(responseData)
+    })
   }
 
   onFetchPosts() {
